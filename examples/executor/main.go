60d276edee5da985888f3a106f4524eabf354964
Abhilash Hegde <hegdeabhilash19@gmail.com>
2024-10-20 11:20:50 +0530

    add more examples

--- examples/executor/main.go
+++ examples/executor/main.go
@@ -7,24 +7,36 @@ import (
 	"time"
 )
 
+var mockSleepDuration = time.Second * 2
+
 func main() {
+
 	now := time.Now()
+
+	// create new executor
 	executor := asyncgo.NewExecutor()
+
 	// submit any function signature
 	// first param is  function, subsequent params are arguments
 	future1 := executor.Submit(func(arg1, arg2 int) int {
 		// mocking delay
-		time.Sleep(1 * time.Second)
+		time.Sleep(mockSleepDuration)
 		return arg1 + arg2
 	}, 10, 20)
 
 	future2 := executor.Submit(func(arg1 int) int {
 		// mocking delay
-		time.Sleep(1 * time.Second)
+		time.Sleep(mockSleepDuration)
 		return arg1 * 10
 	}, 20)
 
-	// getting back the results
+	// you can define a function somewhere and provide function reference with args to execute it asynchronously
+	future3 := executor.Submit(someLongTask, 10)
+
+	// NOTE - err returned by future.Get() represents error that was encountered while executing your function.
+	// It does not represent the error returned by your function
+	// To access error returned by your function you need to convert interface to error type from the result []interface
+
 	result1, err := future1.Get()
 	if err != nil {
 		log.Println(err)
@@ -33,7 +45,16 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	fmt.Println(result1, result2)
-	fmt.Println("time taken %v", time.Since(now))
 
+	result3, err := future3.Get()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(result1, result2, result3)
+	fmt.Println("time taken %v", time.Since(now))
+}
+
+func someLongTask(value int) int {
+	time.Sleep(mockSleepDuration)
+	return value
 }
---
+++ examples/worker_pool/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"github.com/abhi16180/asyncgo"
+	"log"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	exe := asyncgo.NewExecutor()
+	workerPool := exe.NewFixedWorkerPool(context.Background(), &asyncgo.Options{
+		WorkerCount: 50,
+		BufferSize:  10,
+	})
+
+	var futures []*asyncgo.Future
+
+	for i := 0; i < 100; i++ {
+		future, err := workerPool.Submit(someLongTask, i)
+		if err != nil {
+			// this error is thrown if you call this method after shutting down the worker pool
+			log.Printf("error submitting task %d: %v", i, err)
+			break
+		}
+		futures = append(futures, future)
+	}
+	for _, future := range futures {
+		result, err := future.Get()
+		if err != nil {
+			log.Println("error while executing the function", err)
+			continue
+		}
+		log.Println("result", result)
+	}
+	log.Printf("total time taken %v", time.Since(now))
+}
+
+func someLongTask(val int) int {
+	time.Sleep(2 * time.Second)
+	return val
+}
